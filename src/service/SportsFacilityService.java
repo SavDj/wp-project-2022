7d9dc14310ad87a674019f70b3214af3398336cd
SavDj <73643756+SavDj@users.noreply.github.com>
2022-06-18 22:44:41 +0200

    Implemented sports facility search and viewing service

--- src/service/SportsFacilityService.java
+++ src/service/SportsFacilityService.java
@@ -18,5 +18,51 @@ import model.SportsFacility;
 @Path("facilities")
 public class SportsFacilityService {
 	
+	@Context
+	ServletContext ctx;
 	
+	public SportsFacilityService() {}
+	
+	@PostConstruct
+	public void init() {
+		if (ctx.getAttribute("sportsFacilityDao") == null) {
+	    	String contextPath = ctx.getRealPath("");
+			ctx.setAttribute("sportsFacilityDao", new SportsFacilityDAO(contextPath));
+		}
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<SportsFacility> getSportsFacilities() {
+		SportsFacilityDAO dao = (SportsFacilityDAO) ctx.getAttribute("sportsFacilityDao");
+		return dao.getAllSportsFacilities();
+	}
+	
+	@GET
+	@Path("/{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public SportsFacility getSportsFacility(@PathParam("name") String name) {
+		SportsFacilityDAO dao = (SportsFacilityDAO) ctx.getAttribute("sportsFacilityDao");
+		return dao.getFacilityByName(name);
+	}
+	
+	@POST
+	@Path("/search")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<SportsFacility> search(String searchString, String searchType) {
+		SportsFacilityDAO dao = (SportsFacilityDAO) ctx.getAttribute("sportsFacilityDao");
+		switch(searchType) {
+			case "name":
+				return dao.searchByName(searchString);
+			case "type":
+				return dao.searchByType(searchString);
+			case "location":
+				return dao.searchByLocation(searchString);
+			case "rating":
+				return dao.searchByRating(searchString);
+			default:
+				return null;
+		}
+	}
 }
